e066c5417128db0152a8c552cb5422ec75895063
Aditya Lamichhane <adityalamichhane3@gmail.com>
2022-09-15 11:38:21 +0545

    Improvised UI and Enter button implementation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+// import {'../node_modules/@ajusa/lit/dist/lit.css'}
 import './App.css';
 import ToDoList from './ToDoList';
 
@@ -6,6 +7,11 @@ function App (){
     const [inputList, setInputList]=useState("");
 
     const [items, setItems]= useState([]);
+
+    // const [edit, setedit]= useState({
+    //     id:null,
+    //     value:''
+    // });
 
     const itemEvent = (event)=>{
         setInputList(event.target.value);
@@ -15,12 +21,15 @@ function App (){
             window.alert("The text field is empty. Please type a todo.")
         }
         else{
+            console.log(inputList);
         setItems((oldItems) =>{
             return  [...oldItems, inputList];
         })}
         setInputList("");
     
     };
+
+
     const deleteItems = (id) => {
         setItems((oldItems) => {
             return oldItems.filter((arr, index) =>{
@@ -28,29 +37,33 @@ function App (){
             })
         })
     };
-  return (
+  
+    const handleSubmit = e => {
+        if(e.key === "Enter"){
+            listOfItems();
+        }
+    };
+    return (
     <>
     <div className="main">
-    <div className='center'>
-        <h1>To-Do List</h1>
-        <div className='center1'>
-        
-          <input type='text' placeholder='Add a to-do' value={inputList} onChange={itemEvent}/>
-       
-              <button onClick={listOfItems}>
-                  <div className='plis'>
-                   +
-                  </div>
-              </button>
-          </div>  
-          <div className='list'>
-            <ul style={{listStyleType:'none'}}>
-                {items.map( (itemval, index) => {
-                   return <ToDoList key={index} id={index} abc = {itemval} onSelect ={deleteItems}/>;
-                   })}
-            </ul>
-          </div>
-      </div>
+        <div className='center'>
+            <h1>To-Do List</h1>
+            <div className='center1'>      
+                <input type='text' placeholder='Add a to-do' value={inputList} onChange={itemEvent} onKeyPress={handleSubmit}/>
+                <button onClick={listOfItems} >
+                    <i class="fa-solid fa-plus"></i>
+                </button>
+            </div>  
+            <div className='list'>
+                <div className='list1'>          
+                    <ul style={{listStyleType:'none'}}>
+                        {items.map( (itemval, index) => {
+                        return <ToDoList key={index} id={index} text = {itemval} onSelect ={deleteItems}/>;
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </div>
   </div>
   </>
   );
